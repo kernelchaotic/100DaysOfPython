406d46e2ec288bc40a8104185174dd341d48962f
kernelchaotic <115605969+kernelchaotic@users.noreply.github.com>
2022-10-12 05:26:09 +0000

    Add files via upload

---
+++ d01 band name generator.py
@@ -0,0 +1,6 @@
+
+# Day one of 100 days of python: The Band Name Generator
+print("Welcome to the Band Name Generator!")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What animal was your first pet? \n")
+print("Your band name could be " + city_name + " " + pet_name + ".")
