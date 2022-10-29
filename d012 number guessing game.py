3304c29343ab0cd64b0817d03f64f1ea6a32a548
D <115605969+kernelchaotic@users.noreply.github.com>
2022-10-28 18:01:30 -0700

    Update d012 number guessing game.py

--- d012 number guessing game.py
+++ d012 number guessing game.py
@@ -26,94 +26,8 @@ def the_game():
 
     if difficulty == "easy" or difficulty == "easy mode" or difficulty == "e":
         attempts = 10
-        while not game_finished:
-            if attempts == 0:
-                print(f"\nYou've run out of attempts. The number was {number}.")
-                attempts -= 1
-                try_again = input("Try again? Y/N:  ").lower()
-                if try_again == "y" or try_again == "yes":
-                    os.system('clear')
-                    the_game()
-                elif try_again == "n" or try_again == "no":
-                    os.system('clear')
-                    print("Goodbye!")
-                    game_finished = True
-                else:
-                    print("I didn't understand that. Please reboot the program and try again.")
-                    game_finished = True
-            else:
-                print(f"\nYou have {attempts} attempts remaining to guess the number.")
-                player_guess = input("Make a guess:  ")
-                attempts -= 1
-                if player_guess not in range:
-                    print("\nI swear to god if this is another dividing by zero incident I will strangle you.\n")
-                elif int(player_guess) == number:
-                    print(f"You got it! The number was {number}.")
-                    play_again = input("Do you want to play again? Y/N:  ").lower()
-                    if play_again == "y" or play_again == "yes":
-                        os.system('clear')
-                        the_game()
-                    elif play_again == "n" or play_again == "no":
-                        os.system('clear')
-                        print("Goodbye!")
-                        game_finished = True
-                    else:
-                        print("I didn't get that. Terminating process.")
-                        game_finished = True
-                elif int(player_guess) > number:
-                    print("Too high.")
-                    print("Guess again.")
-                elif int(player_guess) < number:
-                    print("Too low.")
-                    print("Guess again.")
-                else:
-                    print("Something broke. Tell D.")
-
     elif difficulty == "hard" or difficulty == "hard mode" or difficulty == "h":
         attempts = 5
-        while not game_finished:
-            if attempts == 0:
-                print(f"\nYou've run out of attempts. The number was {number}.")
-                attempts -= 1
-                try_again = input("Try again? Y/N:  ").lower()
-                if try_again == "y" or try_again == "yes":
-                    os.system('clear')
-                    the_game()
-                elif try_again == "n" or try_again == "no":
-                    os.system('clear')
-                    print("Goodbye!")
-                    game_finished = True
-                else:
-                    print("I didn't understand that. Please reboot the program and try again.")
-                    game_finished = True
-            else:
-                print(f"\nYou have {attempts} attempts remaining to guess the number.")
-                player_guess = input("Make a guess:  ")
-                attempts -= 1
-                if player_guess not in range:
-                    print("\nI swear to god if this is another dividing by zero incident I will strangle you.\n")
-                elif int(player_guess) == number:
-                    print(f"You got it! The number was {number}.")
-                    play_again = input("Do you want to play again? Y/N:  ").lower()
-                    if play_again == "y" or play_again == "yes":
-                        os.system('clear')
-                        the_game()
-                    elif play_again == "n" or play_again == "no":
-                        os.system('clear')
-                        print("Goodbye!")
-                        game_finished = True
-                    else:
-                        print("I didn't get that. Terminating process.")
-                        game_finished = True
-                elif int(player_guess) > number:
-                    print("Too high.")
-                    print("Guess again.")
-                elif int(player_guess) < number:
-                    print("Too low.")
-                    print("Guess again.")
-                else:
-                    print("Something broke. Tell D.")
-
     elif difficulty == "both":
         os.system('clear')
         print("Well we don't always get what we want, do we sweetcheeks? Lower your expectations like the rest of us.")
@@ -125,6 +39,48 @@ def the_game():
         time.sleep(5)
         os.system('clear')
         the_game()
+    while not game_finished:
+        if attempts == 0:
+            print(f"\nYou've run out of attempts. The number was {number}.")
+            attempts -= 1
+            try_again = input("Try again? Y/N:  ").lower()
+            if try_again == "y" or try_again == "yes":
+                os.system('clear')
+                the_game()
+            elif try_again == "n" or try_again == "no":
+                os.system('clear')
+                print("Goodbye!")
+                game_finished = True
+            else:
+                print("I didn't understand that. Please reboot the program and try again.")
+                game_finished = True
+        else:
+            print(f"\nYou have {attempts} attempts remaining to guess the number.")
+            player_guess = input("Make a guess:  ")
+            attempts -= 1
+            if player_guess not in range:
+                print("\nI swear to god if this is another dividing by zero incident I will strangle you.\n")
+            elif int(player_guess) == number:
+                print(f"You got it! The number was {number}.")
+                play_again = input("Do you want to play again? Y/N:  ").lower()
+                if play_again == "y" or play_again == "yes":
+                    os.system('clear')
+                    the_game()
+                elif play_again == "n" or play_again == "no":
+                    os.system('clear')
+                    print("Goodbye!")
+                    game_finished = True
+                else:
+                    print("I didn't get that. Terminating process.")
+                    game_finished = True
+            elif int(player_guess) > number:
+                print("Too high.")
+                print("Guess again.")
+            elif int(player_guess) < number:
+                print("Too low.")
+                print("Guess again.")
+            else:
+                print("Something broke. Tell D.")
 
 
 the_game()
